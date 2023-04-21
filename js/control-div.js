ce9913ad49d41c95b7bcb8c3f0ec72a7d5129116
DragonLJH <1183665002@qq.com>
2023-04-21 18:07:38 +0800

    添加控制组件的关闭方法

---
+++ js/control-div.js
@@ -0,0 +1,79 @@
+Vue.component('control-div', {
+    inject: ['parent'],
+    mixins: [mixin],
+    props: {
+        styleObject: {
+            type: Object
+        },
+        isSetting: {
+            type: Boolean
+        },
+        number: {
+            type: Number
+        }
+    },
+    model: {
+        prop: "styleObject",
+        event: "change"
+    },
+    data() {
+        return {
+            controlStyle: this.styleObject
+        }
+    },
+    mounted() {},
+    methods: {
+        canvasMouseDown(e, direction) {
+            // 获取 control-div 组件的坐标以及宽高
+            const {
+                x: cX,
+                y: cY,
+                width: cW,
+                height: cH
+            } = this.$refs.canvas.getBoundingClientRect()
+            const move = (moveEvent) => {
+                // 解构赋值 给 moveX 和 moveY 分别赋值 clientX 和 clientY
+                // 移动后的坐标
+                const {
+                    clientX: moveX,
+                    clientY: moveY
+                } = moveEvent
+                // 根据拖拽的节点方向（direction）重新计算
+                if (direction) {
+                    let mW = cW
+                    let mH = cH
+                    if (direction.indexOf("r") !== -1) mW += moveX - cX - mW + 20
+                    if (direction.indexOf("b") !== -1) mH += moveY - cY - mH + 20
+                    if (mW + 20 > this.parent.MW) mW = this.parent.MW
+                    if (mH + 20 > this.parent.MH) mH = this.parent.MH
+                    this.controlStyle = {
+                        ...this.controlStyle,
+                        width: mW,
+                        height: mH,
+                    }
+                    return
+                }
+            }
+            const up = (e) => { // 鼠标松开结束事件的监听 
+                document.removeEventListener("mousemove", move);
+                document.removeEventListener("mouseup", up);
+                this.$emit("change", this.controlStyle)
+                console.log("up", this.controlStyle)
+            };
+            // 鼠标按下的时候分别监听鼠标移动事件和鼠标松开事件
+            document.addEventListener("mousemove", move);
+            document.addEventListener("mouseup", up);
+        },
+        close() {
+            this.$emit("index", this.number)
+        }
+
+    },
+    template: `
+        <div class="item" :style='toPx(controlStyle)' ref="canvas">
+            <slot></slot>
+            <div v-show="isSetting" class="icon icon-m guanbi_jiaobiao g-rt" @click="close"></div>
+            <div v-show="isSetting" @mousedown.stop="canvasMouseDown($event, direction)" class="icon icon-m guangbiao-tuozhuai" :class="direction" v-for="(direction, index) in ['r','b','rb',]" :key="index"></div> 
+        </div> 
+    `
+})
